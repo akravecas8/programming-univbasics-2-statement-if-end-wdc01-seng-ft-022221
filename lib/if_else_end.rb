cec313a9c18707a35e7bb213f16428db45375d22
casual-karma-e108 <akravecas@yahoo.com>
2021-02-05 19:04:59 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,4 +5,5 @@ elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
   puts "Pack an umbrella!"
 else
   puts "Stay home and read Hegel."
-end# Write your solution here
+end
+# Write your solution here
